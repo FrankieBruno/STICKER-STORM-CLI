c5981ca272d3ff0d088417e2aaf18be2eac14026
Frankie Bruno <36001544+FrankieBruno@users.noreply.github.com>
2024-02-04 17:51:19 -0600

    update

--- src/views/Authorized.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-
-export const Authorized = () => {
-  if (localStorage.getItem("st_token")) {
-    return <Outlet />
-  }
-  return <Navigate to='/login' replace />
-}
